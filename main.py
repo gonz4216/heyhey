d3a0c723ca0797f95045b11464a32e243130ace4
Ethan Gonsalves <99839612+gonz4216@users.noreply.github.com>
2022-09-05 17:42:02 +0000

    password changer

--- main.py
+++ main.py
@@ -1,2 +1,49 @@
-x = 2 % 4
-print(type(x))
+# def myfun():
+#   message = 'Hey enter password'
+#   print(message)
+#   password_input = input()
+#   print("Password: " + password_input)
+
+# myfun()
+
+
+
+# # message = "Hey Please enter password"
+# # print(message)
+
+# # password_input = input()
+# # print("Password:" + password_input)
+
+
+# # message = "Please confirm password"
+# # print(message)
+
+
+# # password_input2 = input()
+# # print("Password:" + password_input2)
+
+# # if password_input == password_input2:
+# #   print("Congrats on the password change")
+# # else:
+# #   print('Password wasnt the same please try again')
+# #   message = " enter password"
+# # print(message)
+
+# # password_input = input()
+# # print("Password:" + password_input)
+
+
+# # message = "Please confirm password"
+# # print(message)
+
+
+# # password_input2 = input()
+# # print("Password:" + password_input2)
+
+
+username = input("What is your username: ")
+password = input("What is your password: ")
+password_stars = "*" * len(password)
+password_len = len(password)
+print(f"{username}, your password {password_stars} is {password_len} long letters long")
+
