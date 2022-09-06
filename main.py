ca1b4d5a2acd604b185389cbf48753c8f81b14a6
Ethan Gonsalves <99839612+gonz4216@users.noreply.github.com>
2022-09-06 03:15:19 +0000

    counter

--- main.py
+++ main.py
@@ -2,48 +2,67 @@
 #   message = 'Hey enter password'
 #   print(message)
 #   password_input = input()
-#   print("Password: " + password_input)
+# #   print("Password: " + password_input)
 
-# myfun()
-
-
-
-# # message = "Hey Please enter password"
-# # print(message)
-
-# # password_input = input()
-# # print("Password:" + password_input)
+# # myfun()
 
 
-# # message = "Please confirm password"
-# # print(message)
+
+# # # message = "Hey Please enter password"
+# # # print(message)
+
+# # # password_input = input()
+# # # print("Password:" + password_input)
 
 
-# # password_input2 = input()
-# # print("Password:" + password_input2)
-
-# # if password_input == password_input2:
-# #   print("Congrats on the password change")
-# # else:
-# #   print('Password wasnt the same please try again')
-# #   message = " enter password"
-# # print(message)
-
-# # password_input = input()
-# # print("Password:" + password_input)
+# # # message = "Please confirm password"
+# # # print(message)
 
 
-# # message = "Please confirm password"
-# # print(message)
+# # # password_input2 = input()
+# # # print("Password:" + password_input2)
+
+# # # if password_input == password_input2:
+# # #   print("Congrats on the password change")
+# # # else:
+# # #   print('Password wasnt the same please try again')
+# # #   message = " enter password"
+# # # print(message)
+
+# # # password_input = input()
+# # # print("Password:" + password_input)
 
 
-# # password_input2 = input()
-# # print("Password:" + password_input2)
+# # # message = "Please confirm password"
+# # # print(message)
 
 
-username = input("What is your username: ")
-password = input("What is your password: ")
-password_stars = "*" * len(password)
-password_len = len(password)
-print(f"{username}, your password {password_stars} is {password_len} long letters long")
+# # # password_input2 = input()
+# # # print("Password:" + password_input2)
 
+
+# # username = input("What is your username: ")
+# # password = input("What is your password: ")
+# # password_stars = "*" * len(password)
+# # password_len = len(password)
+# # print(f"{username}, your password {password_stars} is {password_len} long letters long")
+
+
+
+
+# def bool_to_word(boolean):
+#     if boolean == True:
+#         return 'Yes'
+#     elif boolean == False:
+#         return 'No'
+
+# bool_to_word(False)
+
+# sum = 0
+# my_list = [1,2,3,4,5,6,7]
+
+# for x in my_list:
+#   sum = sum + x
+  
+# print(sum)
+ 
